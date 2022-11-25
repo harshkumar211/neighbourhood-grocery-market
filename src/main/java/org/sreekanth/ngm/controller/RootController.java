aa5a4169bdf80d056ad118f2be4e2879ac5bc1b9
smondeli <sreekanth.mondeli@oracle.com>
2022-11-25 13:33:17 +0530

    populate heap

--- src/main/java/org/sreekanth/ngm/controller/RootController.java
+++ src/main/java/org/sreekanth/ngm/controller/RootController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.sreekanth.ngm.dao.User;
 import org.sreekanth.ngm.service.ItemService;
-import org.sreekanth.ngm.service.ItemServiceImpl;
+
+import java.util.Random;
 
 @Controller
 public class RootController {
@@ -19,6 +20,7 @@ public class RootController {
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String root(Model model) {
 		model.addAttribute("user", new User());
+		createDummyObjectForTest();
 		return "index";
 	}
 
@@ -27,8 +29,52 @@ public class RootController {
 		model.addAttribute("user", user);
 		model.addAttribute("categories", itemService.listCategories());
 		model.addAttribute("items", itemService.listItems());
-		new ItemServiceImpl().loadItems();
 		return "inventory";
+	}
+
+	private void createDummyObjectForTest() {
+		Runnable r = () -> {
+			for(int i =0; i< 5000; i++){
+				createObject();
+				try {
+					Thread.sleep(new Random().nextInt(2)*1000);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+
+		};
+		new Thread(r).start();
+		new Thread(r).start();
+		new Thread(r).start();
+		new Thread(r).start();
+		new Thread(r).start();
+		new Thread(r).start();
+		new Thread(r).start();
+		new Thread(r).start();
+		new Thread(r).start();
+		new Thread(r).start();
+	}
+
+	private void createObject(){
+		StringBuilder builder = new StringBuilder();
+		for(int i=0; i<200; i++){
+			builder.append(Content.text);
+		}
+		System.out.println("Created Stringbuilder object");
+
+		// Get current size of heap in bytes
+		long heapSize = Runtime.getRuntime().totalMemory();
+
+		// Get maximum size of heap in bytes. The heap cannot grow beyond this size.// Any attempt will result in an OutOfMemoryException.
+		long heapMaxSize = Runtime.getRuntime().maxMemory();
+
+		// Get amount of free memory within the heap in bytes. This size will increase // after garbage collection and decrease as new objects are created.
+		long heapFreeSize = Runtime.getRuntime().freeMemory();
+
+		System.out.println("Current heap size: "+heapSize);
+		System.out.println("Max heap size: "+heapMaxSize);
+		System.out.println("Free heap size: "+heapFreeSize);
 	}
 
 	@RequestMapping(value="/cart", method=RequestMethod.GET)
